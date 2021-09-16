cf28696b0371c32ad165639048bed474c14090ea
steve57000 <73243226+steve57000@users.noreply.github.com>
2021-09-16 12:38:04 +0200

    ok

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,6 +3,7 @@
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under the MIT license
  */
+
 if ("undefined" == typeof jQuery) throw new Error("Bootstrap's JavaScript requires jQuery"); + function(a) {
     "use strict";
     var b = a.fn.jquery.split(" ")[0].split(".");
